1cd0c960608e5cfe5dfc21daca34d2b145c2042f
Mazene <mmazenezerguine@gmail.com>
2024-07-08 16:32:49 +0200

    refactor and adding security

--- api/tasks.py
+++ api/tasks.py
@@ -1,5 +1,4 @@
 import tempfile
-
 from celery import shared_task
 import subprocess
 import uuid
@@ -14,83 +13,74 @@ PHP_AUTOLOAD_PATH = os.getenv('PHP_AUTOLOAD_PATH', '/app/resources/php/vendor/au
 logger = get_task_logger(__name__)
 OUT_DIR = os.path.join(DIR_PATH, 'out/')
 
-
 ENV = os.getenv('ENV', 'dev')
 STATIC_FILES_URL = "http://127.0.0.1:8080/static/" if ENV == "dev" else "http://prod_domain/static/"
 
+def run_docker_command(cmd):
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, timeout=30)
+        return result
+    except subprocess.TimeoutExpired as e:
+        logger.error(f'Execution time exceeded: {str(e)}')
+        return {'error': 'Execution time exceeded'}
+    except subprocess.CalledProcessError as e:
+        logger.error(f'Subprocess error: {str(e)}')
+        return {'error': str(e)}
+    except Exception as e:
+        logger.error(f'Unexpected error: {str(e)}')
+        return {'error': 'Unexpected error occurred'}
 
-STATIC_FILES_URL = ""
-if ENV == "dev":
-    STATIC_FILES_URL = "http://127.0.0.1:8080/static/"
+def get_docker_run_command(language, container_path, unique_id):
+    common_args = [
+        'docker', 'run', '--rm', '--cpus', '1.0', '--memory', '512m', '--memory-swap', '512m',
+        '--read-only', '--pids-limit', '100', '--cap-drop', 'ALL', '--security-opt', 'no-new-privileges',
+        '-v', f'{DIR_PATH}:/app/resources'
+    ]
 
-if ENV == "prod":
-    STATIC_FILES_URL = "http://prod_domain/static/"
-
+    if language == ProgramingLanguagesEnum.PYTHON.value:
+        return common_args + ['code_runner:latest', VENV_PATH, container_path]
+    elif language == ProgramingLanguagesEnum.JAVASCRIPT.value:
+        return common_args + ['code_runner:latest', 'node', container_path]
+    elif language == ProgramingLanguagesEnum.PHP.value:
+        return common_args + ['code_runner:latest', 'php', '-r', f'require "{PHP_AUTOLOAD_PATH}"; include "{container_path}";']
+    elif language == ProgramingLanguagesEnum.CPP.value:
+        compiled_path = f'/app/resources/{language}/compiled_{unique_id}'
+        compile_cmd = common_args + ['code_runner:latest', 'g++', container_path, '-o', compiled_path]
+        compile_result = run_docker_command(compile_cmd)
+        if compile_result.returncode != 0:
+            logger.error(f'Error compiling C++ code: {compile_result.stderr}')
+            return ProgramResultDto(compile_result.stdout, compile_result.stderr, compile_result.returncode).to_dict()
+        return common_args + ['code_runner:latest', compiled_path]
+    else:
+        logger.error(f'Unsupported programming language: {language}')
+        return None
 
 @shared_task
 def run_code(source_code, programming_language):
     unique_id = uuid.uuid4()
-    extension = {
-        'python': 'py',
-        'javascript': 'js',
-        'php': 'php',
-        'c++': 'cpp'
-    }.get(programming_language, 'txt')
+    extension = {'python': 'py', 'javascript': 'js', 'php': 'php', 'c++': 'cpp'}.get(programming_language, 'txt')
     temp_filename = os.path.join(DIR_PATH, f'{programming_language}/code_to_run_{unique_id}.{extension}')
     container_path = f'/app/resources/{programming_language}/code_to_run_{unique_id}.{extension}'
 
     os.makedirs(os.path.dirname(temp_filename), exist_ok=True)
-
     with open(temp_filename, 'w') as f:
         f.write(source_code)
 
-    if programming_language == ProgramingLanguagesEnum.PYTHON.value:
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', VENV_PATH,
-               container_path]
-    elif programming_language == ProgramingLanguagesEnum.JAVASCRIPT.value:
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', 'node',
-               container_path]
-    elif programming_language == ProgramingLanguagesEnum.PHP.value:
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', 'php', '-r',
-               f'require "{PHP_AUTOLOAD_PATH}"; include "{container_path}";']
-    elif programming_language == ProgramingLanguagesEnum.CPP.value:
-        compiled_path = f'/app/resources/{programming_language}/compiled_{unique_id}'
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', 'g++', container_path,
-               '-o', compiled_path]
-        result = subprocess.run(cmd, capture_output=True, text=True, timeout=30)
-        if result.returncode != 0:
-            logger.error(f'Error compiling C++ code: {result.stderr}')
-            return ProgramResultDto(result.stdout, result.stderr, result.returncode).to_dict()
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', compiled_path]
-    else:
-        logger.error(f'Unsupported programming language: {programming_language}')
+    cmd = get_docker_run_command(programming_language, container_path, unique_id)
+    if cmd is None:
         return {'error': 'Unsupported programming language'}
 
-    try:
-        result = subprocess.run(cmd, capture_output=True, text=True, timeout=30)
-        logger.info(f'Execution result for {programming_language} code: {result}')
+    result = run_docker_command(cmd)
 
-        return ProgramResultDto(result.stdout, result.stderr, result.returncode).to_dict()
-    except subprocess.TimeoutExpired:
-        logger.error(f'Execution time exceeded the limit for {programming_language} code')
-        return {'error': 'Execution time exceeded the limit'}
-    except subprocess.CalledProcessError as e:
-        logger.error(f'Error executing {programming_language} code: {e}')
-        return {'error': str(e)}
-    finally:
-        if os.path.exists(temp_filename):
-            os.remove(temp_filename)
+    if os.path.exists(temp_filename):
+        os.remove(temp_filename)
 
+    return ProgramResultDto(result.stdout, result.stderr, result.returncode).to_dict() if isinstance(result, subprocess.CompletedProcess) else result
 
 @shared_task
 def execute_code_with_files(source_code, programming_language, source_files, output_files_formats):
     unique_id = uuid.uuid4()
-    extension = {
-        'python': 'py',
-        'javascript': 'js',
-        'php': 'php',
-        'c++': 'cpp'
-    }.get(programming_language, 'txt')
+    extension = {'python': 'py', 'javascript': 'js', 'php': 'php', 'c++': 'cpp'}.get(programming_language, 'txt')
 
     temp_code_filename = os.path.join(DIR_PATH, f'{programming_language}/code_to_run_{unique_id}.{extension}')
     temp_output_dir = os.path.join(DIR_PATH, 'out')
@@ -99,7 +89,6 @@ def execute_code_with_files(source_code, programming_language, source_files, out
     os.makedirs(os.path.dirname(temp_code_filename), exist_ok=True)
     os.makedirs(temp_output_dir, exist_ok=True)
 
-    # Prepare input files
     container_input_paths = []
     for idx, source_file in enumerate(source_files):
         file_extension = os.path.splitext(source_file)[1]
@@ -111,7 +100,6 @@ def execute_code_with_files(source_code, programming_language, source_files, out
                 input_file.write(src_file.read())
         source_code = source_code.replace(f'INPUT_FILE_PATH_{idx}', f"'{container_input_path}'")
 
-    # Prepare output files if output_files_formats is not empty or null
     output_file_paths = []
     if output_files_formats:
         for idx, file_output_format in enumerate(output_files_formats):
@@ -122,58 +110,31 @@ def execute_code_with_files(source_code, programming_language, source_files, out
     with open(temp_code_filename, 'w') as f:
         f.write(source_code)
 
-    if programming_language == ProgramingLanguagesEnum.PYTHON.value:
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', VENV_PATH,
-               container_code_path]
-    elif programming_language == ProgramingLanguagesEnum.JAVASCRIPT.value:
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', 'node',
-               container_code_path]
-    elif programming_language == ProgramingLanguagesEnum.PHP.value:
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', 'php', '-r',
-               f'require "{PHP_AUTOLOAD_PATH}"; include "{container_code_path}";']
-    elif programming_language == ProgramingLanguagesEnum.CPP.value:
-        compiled_path = f'/app/resources/{programming_language}/compiled_{unique_id}'
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', 'g++',
-               container_code_path, '-o', compiled_path]
-        result = subprocess.run(cmd, capture_output=True, text=True, timeout=30)
-        if result.returncode != 0:
-            logger.error(f'Error compiling C++ code: {result.stderr}')
-            return ProgramResultDto(result.stdout, result.stderr, result.returncode).to_dict()
-        cmd = ['docker', 'run', '--rm', '-v', f'{DIR_PATH}:/app/resources', 'code_runner:latest', compiled_path]
-    else:
-        logger.error(f'Unsupported programming language: {programming_language}')
+    cmd = get_docker_run_command(programming_language, container_code_path, unique_id)
+    if cmd is None:
         return {'error': 'Unsupported programming language'}
 
-    try:
-        result = subprocess.run(cmd, capture_output=True, text=True, timeout=30)
-        logger.info(f'Execution result for {programming_language} code: {result}')
+    result = run_docker_command(cmd)
 
-        output_paths = []
-        if output_file_paths:  # Check if there are output files to process
-            for path in output_file_paths:
-                filename = path.split('/')[-1]
-                local_path = OUT_DIR + filename
-                logger.error(local_path)
-                if os.path.exists(local_path):
-                    output_filename = os.path.basename(local_path)
-                    output_url = STATIC_FILES_URL + output_filename
-                    output_paths.append(output_url)
-                else:
-                    logger.warning(f'Output file not found: {local_path}')
+    output_paths = []
+    if output_file_paths:
+        for path in output_file_paths:
+            filename = path.split('/')[-1]
+            local_path = OUT_DIR + filename
+            logger.error(local_path)
+            if os.path.exists(local_path):
+                output_filename = os.path.basename(local_path)
+                output_url = STATIC_FILES_URL + output_filename
+                output_paths.append(output_url)
+            else:
+                logger.warning(f'Output file not found: {local_path}')
 
-        return ProgramWithFileResultDto(result.stdout, result.stderr, result.returncode, output_paths).to_dict()
-    except subprocess.TimeoutExpired:
-        logger.error(f'Execution time exceeded the limit for {programming_language} code')
-        return {'error': 'Execution time exceeded the limit'}
-    except subprocess.CalledProcessError as e:
-        logger.error(f'Error executing {programming_language} code: {e}')
-        return {'error': str(e)}
-    finally:
-        if os.path.exists(temp_code_filename):
-            os.remove(temp_code_filename)
-        for input_file_path in container_input_paths:
-            input_file_path = os.path.join(DIR_PATH, input_file_path.split('/app/resources/')[1])
-            logger.error(input_file_path)
-            if os.path.exists(input_file_path):
-                os.remove(input_file_path)
+    if os.path.exists(temp_code_filename):
+        os.remove(temp_code_filename)
+    for input_file_path in container_input_paths:
+        input_file_path = os.path.join(DIR_PATH, input_file_path.split('/app/resources/')[1])
+        logger.error(input_file_path)
+        if os.path.exists(input_file_path):
+            os.remove(input_file_path)
 
+    return ProgramWithFileResultDto(result.stdout, result.stderr, result.returncode, output_paths).to_dict() if isinstance(result, subprocess.CompletedProcess) else result
