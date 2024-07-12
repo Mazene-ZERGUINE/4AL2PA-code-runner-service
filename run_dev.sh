c06c345b02fc3ce469f35dd2b08865c6d9e688d6
Mazene <mmazenezerguine@gmail.com>
2024-07-12 23:42:40 +0200

    updating run scripts

--- run_dev.sh
+++ run_dev.sh
@@ -7,10 +7,10 @@ export DJANGO_DEBUG=True
 export ENV=dev
 export SECRET_KEY='django-insecure-7k1r)gx=f87$_77d&^k6r8kj+q-x)l7605^613o61bhif#uxj5'
 export DJANGO_CELERY_BROKER_URL=amqp://localhost
-export S3_BUCKET_NAME=code-runner-service-bucket
-export AWS_ACCESS_KEY_ID=AKIAW3MEA6AXHOZZEGXX
-export AWS_SECRET_ACCESS_KEY=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
-export S3_REGION_NAME=eu-west-1
+#export S3_BUCKET_NAME=
+#export AWS_ACCESS_KEY_ID=
+#export AWS_SECRET_ACCESS_KEY=
+#export S3_REGION_NAME=
 
 echo "Activating virtual environment..."
 source ./venv/bin/activate
--- run_prod.sh
+++ run_prod.sh
@@ -1,6 +1,29 @@
-export DJANGO_DEBUG=False
-export DJANGO_SECRET_KEY="django-insecure-7k1r)gx=f87$_77d&^k6r8kj+q-x)l7605^613o61bhif#uxj5"
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+echo "Setting environment variables..."
+export DJANGO_DEBUG=True
+export ENV=dev
+export SECRET_KEY='django-insecure-7k1r)gx=f87$_77d&^k6r8kj+q-x)l7605^613o61bhif#uxj5'
 export DJANGO_CELERY_BROKER_URL=amqp://localhost
+#export S3_BUCKET_NAME=
+#export AWS_ACCESS_KEY_ID=
+#export AWS_SECRET_ACCESS_KEY=
+#export S3_REGION_NAME=
+
+echo "Activating virtual environment..."
+source ./venv/bin/activate
+
+echo "Installing requirements..."
+pip install -r ./requirements.txt || { echo "Failed to install requirements"; exit 1; }
 
 
-# run process for prod env here
+python manage.py migrate django_celery_results
+python manage.py migrate
+
+
+echo "Starting Django server on port 8080..."
+python manage.py runserver 8080 || { echo "Failed to start Django server"; exit 1; }
+
+echo "Django server started successfully"
