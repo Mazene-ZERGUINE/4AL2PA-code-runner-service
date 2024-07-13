71f3ef920fa5a036613f9d6f3159d8feb4acb7b2
Mazene <mmazenezerguine@gmail.com>
2024-07-13 20:58:47 +0200

    update run prod scriptqs

--- run_celeryapp_prod.sh
+++ run_celeryapp_prod.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-echo "initializing python virtual environment"
-source /venv/bin/activate
-export ENV="production"
+set -e  # Exit immediately if a command exits with a non-zero status.
 
+echo "Setting environment variables..."
+export ENV=production
 
-echo "installing requirements"
-echo " --------------------- "
+echo "Navigating to project directory..."
+cd /path/to/your/project
 
+echo "Checking if virtual environment exists..."
+if [ ! -d "./venv" ]; then
+    echo "Virtual environment not found, creating one..."
+    python3 -m venv venv
+fi
+
+echo "Activating virtual environment..."
+source ./venv/bin/activate
+
+echo "Installing requirements..."
 pip install -r ./requirements.txt || { echo "Failed to install requirements"; exit 1; }
 
-echo "-------------------------"
-
-echo "running celery worker application"
+echo "Starting Celery worker..."
 celery -A code_runner_service worker --loglevel=info
--- run_prod.sh
+++ run_prod.sh
@@ -5,6 +5,11 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 echo "Setting environment variables..."
 export ENV=production
 
+echo "Checking if virtual environment exists..."
+if [ ! -d "./venv" ]; then
+    echo "Virtual environment not found, creating one..."
+    python3 -m venv venv
+fi
 
 echo "Activating virtual environment..."
 source ./venv/bin/activate
@@ -12,15 +17,12 @@ source ./venv/bin/activate
 echo "Installing requirements..."
 pip install -r ./requirements.txt || { echo "Failed to install requirements"; exit 1; }
 
+echo "Running migrations..."
 python manage.py makemigrations
-
-python manage.py migrate django_celery_results
 python manage.py migrate
 
-
-echo "Starting Django server on port 8080..."
+echo "Starting Gunicorn service..."
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
-
 
 echo "Django server started successfully"
